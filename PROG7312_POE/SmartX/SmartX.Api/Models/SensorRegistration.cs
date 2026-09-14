1503107d0884aa7271cfe10c3d522002cc87582d
RamsumarSonali <ST10029895@imconnect.edu.za>
2026-09-14 16:19:12 +0200

    Initialise Smart-X IoT solution
    Created the initial .NET 10 Smart-X Web API project and basic solution structure.

---
+++ PROG7312_POE/SmartX/SmartX.Api/Models/SensorRegistration.cs
@@ -0,0 +1,13 @@
+﻿namespace SmartX.Api.Models
+{
+    public class SensorRegistration
+    {
+        public int Id { get; set; }
+
+        public string DeviceIdentifier { get; set; } = string.Empty;
+
+        public string DeploymentLocation { get; set; } = string.Empty;
+
+        public string Category { get; set; } = string.Empty;
+    }
+}
